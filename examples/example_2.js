de48eff074ac84254589c873332d9b9f97785d79
viclafouch <victor.dlf@outlook.fr>
2019-08-25 16:59:07 +0200

    example_2 with 101apparel.com

---
+++ examples/example_2.js
@@ -0,0 +1,63 @@
+import FetchCrawler from '@viclafouch/fetch-crawler'
+import fs from 'fs'
+
+const urlToCrawl = 'https://101apparel.com/collections/premium-t-shirt-series?page=1'
+let products = []
+
+// I'm getting an array of each product of the page (name, price, images)
+const collectContent = $ => {
+  const products = []
+  $('.product-index').each(function(i, elem) {
+    products[i] = {
+      name: $(this)
+        .find($('.product-info-inner a'))
+        .text()
+        .replace(/(\r\n|\n|\r)/gm, '')
+        .trim(),
+      price: parseInt(
+        $(this)
+          .find($('.product-info-inner .price'))
+          .text()
+          .replace(/(\r\n|\n|\r|[^\d.-])/gm, '')
+          .trim()
+      ),
+      images: $(this)
+        .find($('img'))
+        .map((i, el) => $(el).attr('src'))
+        .get()
+        .filter(src => src.includes('/products/'))
+    }
+  })
+  return products
+}
+
+// Only url including an exact string
+const checkUrl = url => (url.includes('premium-t-shirt-series?page=') ? url : false)
+
+// Concat my new products to my array
+const doSomethingWith = (content, url) => (products = products.concat(content))
+
+// Await for the crawler, and then save result in a JSON file
+;(async () => {
+  await FetchCrawler.launch({
+    url: urlToCrawl,
+    evaluatePage: $ => collectContent($),
+    onSuccess: ({ result, url }) => doSomethingWith(result, url),
+    preRequest: url => checkUrl(url),
+    maxDepth: 4,
+    parallel: 6
+  })
+
+  const jsonResult = JSON.stringify({ ...products }, null, 2)
+
+  try {
+    await new Promise((resolve, reject) =>
+      fs.writeFile('examples/example_2.json', jsonResult, err => {
+        if (err) reject(err)
+        else resolve()
+      })
+    )
+  } catch (error) {
+    console.error(error)
+  }
+})()
